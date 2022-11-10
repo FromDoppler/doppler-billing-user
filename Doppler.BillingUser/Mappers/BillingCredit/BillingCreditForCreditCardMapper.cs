0054bdcdf6c99b3a36c6e758f95b9a78e88c9f9f
jhoffman-ms <jmh248@gmail.com>
2022-11-10 10:08:08 -0300

    feat: Set 1 in the discountId

--- Doppler.BillingUser/Mappers/BillingCredit/BillingCreditForCreditCardMapper.cs
+++ Doppler.BillingUser/Mappers/BillingCredit/BillingCreditForCreditCardMapper.cs
@@ -65,7 +65,7 @@ namespace Doppler.BillingUser.Mappers.BillingCredit
             {
                 var planDiscountInformation = await _billingRepository.GetPlanDiscountInformation(agreementInformation.DiscountId);
 
-                buyCreditAgreement.BillingCredit.IdDiscountPlan = agreementInformation.DiscountId != 0 ? agreementInformation.DiscountId : null;
+                buyCreditAgreement.BillingCredit.IdDiscountPlan = agreementInformation.DiscountId != 0 ? agreementInformation.DiscountId : 1;
                 buyCreditAgreement.BillingCredit.TotalMonthPlan = planDiscountInformation != null ? planDiscountInformation.MonthPlan : 1;
                 buyCreditAgreement.BillingCredit.CurrentMonthPlan = currentBillingCredit == null ?
                     (buyCreditAgreement.BillingCredit.TotalMonthPlan.HasValue
