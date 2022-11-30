d18db413e64735a355e3746d84003dbce946ab9b
ms-fcabrera <fcabrera@makingsense.com>
2022-11-30 11:30:44 -0300

    fix: add test to verify that paymentStatusEnums were not modified

--- Doppler.BillingUser/Mappers/PaymentStatus/PaymentStatusMapper.cs
+++ Doppler.BillingUser/Mappers/PaymentStatus/PaymentStatusMapper.cs
@@ -1,5 +1,6 @@
 using Doppler.BillingUser.ApiModels;
 using Doppler.BillingUser.Enums;
+using System;
 using System.Collections;
 
 namespace Doppler.BillingUser.Mappers.PaymentStatus
@@ -12,7 +13,19 @@ namespace Doppler.BillingUser.Mappers.PaymentStatus
             {
                 PaymentStatusApiEnum.Approved => PaymentStatusEnum.Approved,
                 PaymentStatusApiEnum.Pending => PaymentStatusEnum.Pending,
-                PaymentStatusApiEnum.Declined => PaymentStatusEnum.DeclinedPaymentTransaction
+                PaymentStatusApiEnum.Declined => PaymentStatusEnum.DeclinedPaymentTransaction,
+                _ => throw new ArgumentException($"Unexpected value `{status}`", nameof(status))
+            };
+        }
+
+        public PaymentStatusApiEnum MapFromPaymentStatusEnumToPaymentStatusApiEnum(PaymentStatusEnum status)
+        {
+            return status switch
+            {
+                PaymentStatusEnum.Approved => PaymentStatusApiEnum.Approved,
+                PaymentStatusEnum.Pending => PaymentStatusApiEnum.Pending,
+                PaymentStatusEnum.DeclinedPaymentTransaction => PaymentStatusApiEnum.Declined,
+                _ => throw new ArgumentException($"Unexpected value `{status}`", nameof(status))
             };
         }
 
