2d7a6b35d6df2f522506d157e0a82fb8020658f9
ms-fcabrera <fcabrera@makingsense.com>
2023-04-21 10:32:02 -0300

    fix: repair tests

--- Doppler.BillingUser.Test/GetCurrentPaymentMethodTest.cs
+++ Doppler.BillingUser.Test/GetCurrentPaymentMethodTest.cs
@@ -110,7 +110,7 @@ namespace Doppler.BillingUser.Test
         public async Task GET_current_payment_method_should_get_right_values_when_username_is_valid()
         {
             // Arrange
-            const string expectedContent = "{\"ccHolderFullName\":\"TEST\",\"ccNumber\":\"TEST\",\"ccVerification\":\"****\",\"ccExpMonth\":\"2\",\"ccExpYear\":\"2130\",\"ccType\":\"Visa\",\"paymentMethodName\":\"CC\",\"renewalMonth\":\"6\",\"razonSocial\":\"Company\",\"idConsumerType\":\"NC\",\"identificationType\":\"DNI\",\"identificationNumber\":\"344444\",\"idSelectedPlan\":1,\"responsableIVA\":true,\"idCCType\":0,\"useCFDI\":null,\"paymentType\":null,\"paymentWay\":null,\"bankName\":null,\"bankAccount\":null}";
+            const string expectedContent = "{\"ccHolderFullName\":\"TEST\",\"ccNumber\":\"TEST\",\"ccVerification\":\"****\",\"ccExpMonth\":\"2\",\"ccExpYear\":\"2130\",\"ccType\":\"Visa\",\"paymentMethodName\":\"CC\",\"renewalMonth\":\"6\",\"razonSocial\":\"Company\",\"idConsumerType\":\"NC\",\"identificationType\":\"DNI\",\"identificationNumber\":\"344444\",\"idSelectedPlan\":1,\"responsableIVA\":true,\"idCCType\":0,\"useCFDI\":null,\"paymentType\":null,\"paymentWay\":null,\"bankName\":null,\"bankAccount\":null,\"taxRegime\":0}";
             var paymentMethod = new PaymentMethod
             {
                 PaymentMethodName = "CC",
@@ -126,7 +126,8 @@ namespace Doppler.BillingUser.Test
                 RazonSocial = "Company",
                 RenewalMonth = "6",
                 IdSelectedPlan = 1,
-                ResponsableIVA = true
+                ResponsableIVA = true,
+                TaxRegime = 0,
             };
 
             var mockConnection = new Mock<DbConnection>();
