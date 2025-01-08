6afbfbb0e829310856caa1f8758a7adf226083f3
HomeVarela <homecavs@gmail.com>
2025-01-08 17:25:53 -0300

    feat: create bin api service

---
+++ Doppler.BillingUser/ExternalServices/BinApi/CardNotFoundException.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Doppler.BillingUser.ExternalServices.BinApi
+{
+    public class CardNotFoundException : Exception
+    {
+        public CardNotFoundException() : base() {}
+    }
+}
