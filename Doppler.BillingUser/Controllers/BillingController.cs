72c584bdec54d12bbaca9c23f17cb3bd7198fdff
homecavs <homecavs@gmail.com>
2025-04-07 17:13:30 -0300

    feat: add endpoint for cancelling push notification plan

--- Doppler.BillingUser/Controllers/BillingController.cs
+++ Doppler.BillingUser/Controllers/BillingController.cs
@@ -2117,6 +2117,53 @@ namespace Doppler.BillingUser.Controllers
         }
 
         [Authorize(Policies.OWN_RESOURCE_OR_SUPERUSER)]
+        [HttpPut("/accounts/{accountname}/pushnotification/cancel")]
+        public async Task<IActionResult> CancenlCurrentPushNotificationPlan(string accountname)
+        {
+            User user = await _userRepository.GetUserInformation(accountname);
+
+            if (user == null)
+            {
+                return new NotFoundObjectResult("The user does not exist");
+            }
+
+            var userType = !user.IdClientManager.HasValue ? "REG" : "CM";
+
+            try
+            {
+                UserAddOn userAddOn = await _userAddOnRepository.GetByUserIdAndAddOnType(user.IdUser, (int)AddOnType.PushNotification);
+
+                if (userAddOn == null)
+                {
+                    return new NotFoundObjectResult("The user does not have any push notification plan");
+                }
+
+                var currentPushNotificationPlanBillingCredit = await _billingRepository.GetBillingCredit(userAddOn.IdCurrentBillingCredit);
+                if (currentPushNotificationPlanBillingCredit != null && currentPushNotificationPlanBillingCredit.IdBillingCreditType != (int)BillingCreditTypeEnum.PushNotification_Canceled)
+                {
+                    await _billingRepository.UpdateBillingCreditType(userAddOn.IdCurrentBillingCredit, (int)BillingCreditTypeEnum.PushNotification_Canceled);
+                }
+
+                var message = $"{userType} - Successful cancel push notification plan for: User: {accountname}";
+                _logger.LogError(message);
+                await _slackService.SendNotification(message);
+
+                return new OkObjectResult(message);
+            }
+            catch
+            {
+                var message = $"{userType} - Failed at canceling push notification plan for: User: {accountname}";
+                _logger.LogError(message);
+                await _slackService.SendNotification(message);
+
+                return new ObjectResult(message)
+                {
+                    StatusCode = 500,
+                };
+            }
+        }
+
+        [Authorize(Policies.OWN_RESOURCE_OR_SUPERUSER)]
         [HttpPost("/accounts/{accountname}/{addOnType}/activate")]
         public async Task<IActionResult> ActivateAddOnPlan(string accountname, AddOnType addOnType)
         {
