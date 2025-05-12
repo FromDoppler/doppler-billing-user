e8812b9a32783cd6e27c36fce0bd85c586004d7b
jhoffman-ms <jmh248@gmail.com>
2025-05-12 09:34:31 -0300

    feat: add cbu validation

---
+++ Doppler.BillingUser/Model/PayrollOfBCRAEntity.cs
@@ -0,0 +1,8 @@
+namespace Doppler.BillingUser.Model
+{
+    public class PayrollOfBCRAEntity
+    {
+        public string BankCode { get; set; }
+        public string BankName { get; set; }
+    }
+}
