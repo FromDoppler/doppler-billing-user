cc48c6e5500e8e4b4e0e51c3d0c571077e231dd5
jhoffman-ms <jmh248@gmail.com>
2025-10-29 14:40:19 -0300

    feat: integrate with purchase endpoint

--- Doppler.BillingUser/ExternalServices/PaymentsApi/IPaymentsService.cs
+++ Doppler.BillingUser/ExternalServices/PaymentsApi/IPaymentsService.cs
@@ -5,5 +5,6 @@ namespace Doppler.BillingUser.ExternalServices.PaymentsApi
     public interface IPaymentsService
     {
         Task<string> GeneratePaymentToken(string WorldPayLowValueToken, string CardNumber);
+        Task<string> Purchase(string paymentToken, decimal amount);
     }
 }
